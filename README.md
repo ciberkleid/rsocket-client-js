8906ddcbb55dbd749ecaf2e2ceddf0eb0997afba
Cora Iberkleid <ciberkleid@pivotal.io>
2019-09-16 22:48:02 -0400

    Set SERVER_URL in src/config.js. Use config.sh to create the file

--- README.md
+++ README.md
@@ -5,10 +5,23 @@ This is an RSocket JavaScript WebSocket client.
 This demo works in tandem with [rsocket-server](https://github.com/ciberkleid/rsocket-server).
 Please see the rsocket-server [README](https://github.com/ciberkleid/rsocket-server/blob/master/README.md) for instructions on running the demo.
 
-To run locally:
+To use this client app, first clone the repo:
 ```
 git clone https://github.com/ciberkleid/rsocket-client-js.git
 cd rsocket-client-js
+```
+
+Create a file called '.env' in the rsocket-client-js directory.
+Add the line below to set SERVER_URL.
+Update the value as appropriate.
+Make sure to include the rsocket-server path-mapping (the final '/' in the example below).
+
+SERVER_URL='ws://my-server.my-domain.com/'
+
+Then run the following commands:
+
+```
+source config.sh
 npm install
 npm run build
 ```
--- config.sh
+++ config.sh
@@ -2,7 +2,20 @@
 # It ignores comments lines, replaces '=' with ': ', and prepends each line with two spaces
 # It also wraps the values copied from .env file in a module.exports template
 
-printf 'module.exports = {\n' > src/config.js
-sed 's/^#.*//' .env | sed 's/=/: /' | sed 's/^/  /' >> src/config.js
-printf '\n}\n' >> src/config.js
-
+FILE=.env
+if [ -f "$FILE" ]; then
+  echo "Creating src/config.js using contents of existing $FILE file"
+  printf 'module.exports = {\n' > src/config.js
+  sed 's/^#.*//' .env | sed 's/=/: /' | sed 's/^/  /' >> src/config.js
+  printf '}\n' >> src/config.js
+  return 0
+else
+  echo ""
+  echo "ERROR: File $FILE does not exist." >> /dev/stderr
+  echo "Create a file called '.env'. Add the line below to set SERVER_URL."
+  echo "Update value as appropriate. Include server path-mapping (final '/' in example below)."
+  echo ""
+  echo "SERVER_URL='ws://my-server.my-domain.com/'"
+  echo ""
+  return 1
+fi
